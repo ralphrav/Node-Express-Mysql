6ad67995f576131050b675e839d5b3d1c955008d
Ralph Villa <ralph.villa03@gmail.com>
2018-03-30 02:35:45 +0800

    Commit #3

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const router  = express.Router();
-const mysql = require('mysql');
+const express    = require('express');
+const router     = express.Router();
+const mysql      = require('mysql');
 const bodyparser = require('body-parser');
 
 //Database Connection
@@ -21,18 +21,8 @@ db.connect((err) => {
 
 const app = express();
 
-app.get('/sampleGet', (req, res) => {
-	var id = req.query.id;
-	res.send(id);
-});
-
-app.post('/samplePost', (req, res) => {
-	var id = req.query.id;
-	res.send(id);
-});
-
 //Create Database
-app.get('/createdb', (req, res) => {
+app.get('/createDB', (req, res) => {
 	let sql = 'CREATE DATABASE nodemysql';
 	db.query(sql, (err, result) => {
 		if(err) throw err;
@@ -42,7 +32,7 @@ app.get('/createdb', (req, res) => {
 });
 
 //Create table
-app.get('/createposttable', (req, res) => {
+app.get('/createTable', (req, res) => {
 	let sql = 'CREATE TABLE post(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
 	db.query(sql, (err, result) => {
 		if(err) throw err;
@@ -52,7 +42,7 @@ app.get('/createposttable', (req, res) => {
 });
 
 //Insert data
-app.get('/addpost', (req, res) => {
+app.get('/addPost', (req, res) => {
 	var title = req.query.title;
 	var body  = req.query.body;
 	let post  = {title: title, body: body};
@@ -65,7 +55,7 @@ app.get('/addpost', (req, res) => {
 });
 
 //Select post
-app.get('/getpost', (req, res) => {	
+app.get('/getPost', (req, res) => {	
 	let sql   = 'SELECT * FROM post';
 	let query = db.query(sql, (err, result) =>{
 		if(err) throw err;
@@ -75,7 +65,7 @@ app.get('/getpost', (req, res) => {
 });
 
 //Select Single post
-app.get('/singlePost/:id', (req, res) => {	
+app.get('/getSinglePost/:id', (req, res) => {	
 	let sql   = `SELECT * FROM post where id = ${req.params.id}`;
 	let query = db.query(sql, (err, result) =>{
 		if(err) throw err;
@@ -85,10 +75,10 @@ app.get('/singlePost/:id', (req, res) => {
 });
 
 //Update post
-app.get('/updatepost/:id', (req, res) => {
+app.get('/updatePost/:id', (req, res) => {
 	let newTitle = 'Updated Title';
 	let sql      = `UPDATE post SET title = '${newTitle}' WHERE id = ${req.params.id}`;
-	let query = db.query(sql, (err, result) =>{
+	let query    = db.query(sql, (err, result) =>{
 		if(err) throw err;
 		console.log(result);
 		res.send('Post fetched');
@@ -96,10 +86,10 @@ app.get('/updatepost/:id', (req, res) => {
 });
 
 //Delete post
-app.get('/deletepost/:id', (req, res) => {
+app.get('/deletePost/:id', (req, res) => {
 	let newTitle = 'Updated Title';
 	let sql      = `DELETE FROM post WHERE id = ${req.params.id}`;
-	let query = db.query(sql, (err, result) =>{
+	let query    = db.query(sql, (err, result) =>{
 		if(err) throw err;
 		console.log(result);
 		res.send('Post Deleted');
